16a48a83cb4b38b1403a64a3f5c10c0755be4ee5
saintzzDev <rafagha.eb@gmail.com>
2026-08-19 11:48:58 -0700

    agregar vercel.json

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,4 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/rest-countries-api-with-color-theme-switcher-master'
 })
